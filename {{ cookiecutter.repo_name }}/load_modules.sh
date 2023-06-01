e0518734feb3ae95e6168c10588ce44b72916184
Rob Beagrie <rob@beagrie.co.uk>
2023-06-01 17:57:11 +0100

    Added ability to bootstrap python virtualenvs

---
+++ {{ cookiecutter.repo_name }}/load_modules.sh
@@ -0,0 +1,2 @@
+module purge
+module load Python/3.9.5-GCCcore-10.3.0
--- {{ cookiecutter.repo_name }}/run_server.sh
+++ {{ cookiecutter.repo_name }}/run_server.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 port_num=$(./get_port.sh)
-# source env-${MODULE_CPU_TYPE}/bin/activate
-echo jupyter lab --port $port_num --no-browser
+source env-${MODULE_CPU_TYPE}/bin/activate
+jupyter lab --port $port_num --no-browser
