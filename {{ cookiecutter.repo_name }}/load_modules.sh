c6908c5cccf7765a6c6186abfa99f626f91f6d9f
Rob Beagrie <rob@beagrie.co.uk>
2023-06-02 11:37:38 +0100

    Add R to modules

--- {{ cookiecutter.repo_name }}/load_modules.sh
+++ {{ cookiecutter.repo_name }}/load_modules.sh
@@ -4,3 +4,4 @@ module load git/2.32.0-GCCcore-10.3.0-nodocs
 module load Bowtie2/2.4.4-GCC-10.3.0
 module load samtools/1.8-gcc5.4.0
 module load BEDTools/2.30.0-GCC-10.3.0
+module load R-bundle-Bioconductor/3.13-foss-2021a-R-4.1.0
