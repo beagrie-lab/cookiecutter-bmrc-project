2e9d4071c84bbb43752cbcbcf577d0704fa25c5f
Rob Beagrie <rob@beagrie.co.uk>
2023-06-02 11:20:39 +0100

    Add more modules to be loaded

--- {{ cookiecutter.repo_name }}/load_modules.sh
+++ {{ cookiecutter.repo_name }}/load_modules.sh
@@ -1,2 +1,6 @@
 module purge
 module load Python/3.9.5-GCCcore-10.3.0
+module load git/2.32.0-GCCcore-10.3.0-nodocs
+module load Bowtie2/2.4.4-GCC-10.3.0
+module load samtools/1.8-gcc5.4.0
+module load BEDTools/2.30.0-GCC-10.3.0
