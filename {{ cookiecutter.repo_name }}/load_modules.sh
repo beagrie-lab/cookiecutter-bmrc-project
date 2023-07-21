af98579c7239aeac86b41f77aadaacf4552c244c
Rob Beagrie <rob@beagrie.co.uk>
2023-07-21 09:59:10 +0100

    Added gamtools support

--- {{ cookiecutter.repo_name }}/load_modules.sh
+++ {{ cookiecutter.repo_name }}/load_modules.sh
@@ -1,7 +1,5 @@
 module purge
-module load Python/3.9.5-GCCcore-10.3.0
-module load git/2.32.0-GCCcore-10.3.0-nodocs
-module load Bowtie2/2.4.4-GCC-10.3.0
-module load samtools/1.8-gcc5.4.0
-module load BEDTools/2.30.0-GCC-10.3.0
-module load R-bundle-Bioconductor/3.13-foss-2021a-R-4.1.0
+module load R/4.2.1-foss-2022a
+module load Python/3.10.4-GCCcore-11.3.0
+module load git/2.36.0-GCCcore-11.3.0-nodocs
+module load SAMtools/1.16.1-GCC-11.3.0
