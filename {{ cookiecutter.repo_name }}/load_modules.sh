eb2ed35e77edcbb28d9b3f33154af885e344847b
Rob Beagrie <rob@beagrie.co.uk>
2024-09-16 15:21:55 +0100

    Updated to new modules

--- {{ cookiecutter.repo_name }}/load_modules.sh
+++ {{ cookiecutter.repo_name }}/load_modules.sh
@@ -1,5 +1,5 @@
 module purge
-module load R/4.2.1-foss-2022a
-module load Python/3.10.4-GCCcore-11.3.0
-module load git/2.36.0-GCCcore-11.3.0-nodocs
-module load SAMtools/1.16.1-GCC-11.3.0
+module load Python/3.11.3-GCCcore-12.3.0
+module load git/2.41.0-GCCcore-12.3.0-nodocs
+module load SAMtools/1.18-GCC-12.3.0
+module load R/4.3.2-gfbf-2023a
